69f37eb9e0d1f8a7eb60443e683cdb120f70dda4
kebin20 <ktanzyl@hotmail.com>
2023-03-22 12:42:44 +0900

    2 files modified

--- src/flashcard-data.tsx
+++
@@ -1,6 +0,0 @@
-import XLSX from 'xlsx';
-const workbook = XLSX.readFile('flashcards.xlsx');
-const sheetName = workbook.SheetNames[0];
-const worksheet = workbook.Sheets[sheetName];
-const data = XLSX.utils.sheet_to_json(worksheet);
-console.log(JSON.stringify(data));
