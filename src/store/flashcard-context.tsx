3e3449bff2746fba5f04188e4f0fab43423192fa
kebin20 <ktanzyl@hotmail.com>
2023-04-21 12:03:14 +0900

    feat: added a context for repeating functions and values

---
+++ src/store/flashcard-context.tsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+
+import { CardContentType } from '../interfaces';
+
+type FlashcardContextObj = {
+  flashcard: CardContentType[];
+  goForward: () => void;
+  goBack: () => void;
+  handleKeyDown: (event: { code: string }) => void;
+  vocabLearnt: () => void;
+  reviseVocab: () => void;
+  reset: () => void;
+  flipCard: () => void;
+};
+
+export const FlashcardContext = React.createContext<FlashcardContextObj>({
+  flashcard: [],
+  goForward: () => {},
+  goBack: () => {},
+  handleKeyDown: (event: { code: string }) => {},
+  vocabLearnt: () => {},
+  reviseVocab: () => {},
+  reset: () => {},
+  flipCard: () => {},
+});
+
