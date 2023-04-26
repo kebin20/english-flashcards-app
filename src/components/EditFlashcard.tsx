1ac6d2d63ce56192868a342e59c9d72903d7fd4a
kebin20 <ktanzyl@hotmail.com>
2023-04-26 10:03:46 +0900

    chore: attempt at rendering vocabs in list

--- src/components/EditFlashcard.tsx
+++ src/components/EditFlashcard.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import { Editable, EditableInput, EditablePreview } from '@chakra-ui/editable';
+import { CardContentType } from '../interfaces';
 
 const StyledEditCardContainer = styled.div`
   background-color: var(--clr-light);
@@ -23,10 +24,16 @@ const StyledEditCardContainer = styled.div`
 `;
 
 function FlashcardItem(props: any) {
+  // const [cardText, setCardText] = useState(
+  //   props.deckCards.map((card: CardContentType) => ({
+  //     fuText: card.furigana,
+  //     enText: card.english,
+  //     jpText: card.japanese,
+  //   }))
+  // );
+
   const [value, setValue] = useState('');
   const [isEditing, setIsEditing] = useState(false);
-
-  console.log(value);
 
   function handleOnEdit() {
     setIsEditing(true);
@@ -43,93 +50,26 @@ function FlashcardItem(props: any) {
 
   return (
     <>
-      <StyledEditCardContainer>
-        <Editable
-          defaultValue="Type here"
-          isPreviewFocusable={!isEditing}
-          onEdit={handleOnEdit}
-          onSubmit={handleOnSubmit}
-          onCancel={handleOnCancel}
-        >
-          <EditablePreview
-            sx={{
-              display: isEditing ? 'none' : 'initial',
-            }}
-          ></EditablePreview>
-          <EditableInput />
-        </Editable>
-      </StyledEditCardContainer>
+      {/* {props.deckCards.map((card: CardContentType, index: number) => ( */}
+        <StyledEditCardContainer>
+          <Editable
+            defaultValue="Type here"
+            isPreviewFocusable={!isEditing}
+            onEdit={handleOnEdit}
+            onSubmit={handleOnSubmit}
+            onCancel={handleOnCancel}
+          >
+            <EditablePreview
+              sx={{
+                display: isEditing ? 'none' : 'initial',
+              }}
+            ></EditablePreview>
+            <EditableInput />
+          </Editable>
+        </StyledEditCardContainer>
+      {/* ))} */}
     </>
   );
 }
 
 export default FlashcardItem;
-
-// State for the inputs
-// const [cardText, setCardText] = useState(
-//   props.deckCards.map((card: CardsContentType) => ({
-//     fuText: card.furigana,
-//     enText: card.english,
-//     jpText: card.japanese,
-//   }))
-// );
-
-// const handleInputChange = (index: number, field: string, value: string) => {
-//   const newCardText = [...cardText];
-//   newCardText[index][field] = value;
-//   setCardText(newCardText);
-// };
-
-{
-  /* {props.deckCards.map((card: CardsContentType, index: number) => (
-        <StyledFlashcardContainer key={card.id}>
-          <span>{card.cardNumber}.</span>
-          <Editable
-            text={card.japanese}
-            placeholder={card.japanese}
-            type="input"
-          >
-            <input
-              type="text"
-              name="japanese input"
-              placeholder=""
-              value={cardText[index].jpText}
-              onChange={(e) =>
-                handleInputChange(index, 'jpText', e.target.value)
-              }
-            />
-          </Editable>
-          <Editable
-            text={card.furigana}
-            placeholder={card.furigana}
-            type="input"
-          >
-            <input
-              type="text"
-              name="furigana input"
-              placeholder={card.furigana}
-              value={cardText[index].fuText}
-              onChange={(e) =>
-                handleInputChange(index, 'fuText', e.target.value)
-              }
-            />
-          </Editable>
-          <Editable
-            text={card.english}
-            placeholder={card.japanese}
-            type="input"
-          >
-            <input
-              type="text"
-              name="task"
-              placeholder={card.english}
-              value={cardText[index].enText}
-              onChange={(e) =>
-                handleInputChange(index, 'enText', e.target.value)
-              }
-            />
-          </Editable>
-          <button>T</button>
-        </StyledFlashcardContainer>
-      ))} */
-}
